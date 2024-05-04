9b9533345e1873755577d6bf4491f0f619e55e5e
ak-horiuchi <ak-horiuchi@respect-pal.jp>
2024-05-04 21:49:32 +0900

    bootstrap導入、チャットルームページのスタイルなどを調整

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,5 +5,15 @@ class RoomsController < ApplicationController
   end
 
   def show
+    @room = Room.find(params[:id])
+
+    # roomメンバーでなければ追い出す
+    unless @room.users.pluck(:id).include?(current_user.id)
+      redirect_to rooms_path
+    end
+
+    @messages = Message.where(room: @room)
+                       .order(created_at: :desc)
+
   end
 end
