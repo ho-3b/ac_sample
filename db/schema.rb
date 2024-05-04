fb947544e3f3aa66e9e701e2e00058b9a30af45a
ak-horiuchi <ak-horiuchi@respect-pal.jp>
2024-05-04 15:39:50 +0900

    中間テーブル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_04_063609) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_063832) do
+  create_table "chat_members", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_chat_members_on_room_id"
+    t.index ["user_id"], name: "index_chat_members_on_user_id"
+  end
+
   create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -26,4 +35,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_04_063609) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "chat_members", "rooms"
+  add_foreign_key "chat_members", "users"
 end
